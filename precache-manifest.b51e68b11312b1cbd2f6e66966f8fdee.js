7dd56a21e6891512bb7a2fc8259dec9eafd1089d
cdix1313 <sidik.saepudin13@gmail.com>
2020-02-27 15:28:56 +0700

    Updates

---
+++ precache-manifest.b51e68b11312b1cbd2f6e66966f8fdee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd83e90313c0fcf4cbeb94abafd1fa52",
+    "url": "/resto-finder%60/index.html"
+  },
+  {
+    "revision": "513c3e1771a1383456b3",
+    "url": "/resto-finder%60/static/css/main.b253d4ba.chunk.css"
+  },
+  {
+    "revision": "164a9bb1d79ffd92b7ff",
+    "url": "/resto-finder%60/static/js/2.c86de4dd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/resto-finder%60/static/js/2.c86de4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513c3e1771a1383456b3",
+    "url": "/resto-finder%60/static/js/main.89cc5a08.chunk.js"
+  },
+  {
+    "revision": "0506d65d965e86292705",
+    "url": "/resto-finder%60/static/js/runtime-main.9b18ab43.js"
+  },
+  {
+    "revision": "f3483f64fe4f53928cfe2786e7ec82cc",
+    "url": "/resto-finder%60/static/media/FuturaPTBold.f3483f64.otf"
+  },
+  {
+    "revision": "99541f31c3aca8a3251b82b28ec7966e",
+    "url": "/resto-finder%60/static/media/FuturaPTBook.99541f31.otf"
+  },
+  {
+    "revision": "5b6575ccd521b96b68504700b10e3e66",
+    "url": "/resto-finder%60/static/media/FuturaPTDemi.5b6575cc.otf"
+  },
+  {
+    "revision": "a15258a71bcff8eb791ad54f27144d48",
+    "url": "/resto-finder%60/static/media/FuturaPTLight.a15258a7.otf"
+  },
+  {
+    "revision": "bd99f124d37cfd9caecec4fe24094334",
+    "url": "/resto-finder%60/static/media/FuturaPTMedium.bd99f124.otf"
+  }
+]);
